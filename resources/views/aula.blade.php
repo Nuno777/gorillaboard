6ee57d6a8c6dd15eaa846ef9f2832e36f8a5c32f
Nuno Miguel <nm.albano20@gmail.com>
2022-11-17 17:06:51 +0000

    Auto stash before merge of "main" and "origin/main"

--- resources/views/aula.blade.php
+++ resources/views/aula.blade.php
@@ -1,4 +1,23 @@
 @extends('layout.master')
 @section('main')
-
+<div class="row">
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
