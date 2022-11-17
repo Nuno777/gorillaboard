c972e0baf8a1e2809a4e1d97fdb8bc37e603dbee
Nuno Miguel <nm.albano20@gmail.com>
2022-11-17 17:18:05 +0000

    nav, head and footer

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,49 +2,14 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="{{ asset('css/homepage_Style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/stylesobre.css') }}">
-
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-    <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-    <!-- MDB -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.0/mdb.min.css" rel="stylesheet" />
-
+    @include('layout.partials.header')
 </head>
 
 <body>
 
-    <nav>
-        <ul id="navbar">
-            <li><a id="profile_Icon_Box" href="#"><img id="profile_Icon" src="{{ asset('img/profile_icon.png') }}"
-                        alt=""></a></li>
-            <li id="li_Logo"><a href="/"><img id="nav_Logo" src="{{ asset('img/gorilla_horizontal.png') }}"
-                        alt=""></a></li>
-            <div class="nav_option">
-                <li><a id="phone_Group" href="#"><img id="phone_Icon" src="{{ asset('img/phone_icon.png') }}"
-                            alt="">266123123</a></li>
-                <li><a class="active" href="{{route('sobre')}}">Sobre</a></li>
-                <li><a c href="#news">Horarios</a></li>
-                <li><a href="{{ route('aula') }}">Aulas</a></li>
-                <li><a href="#about">Desportos</a></li>
-
-            </div>
-        </ul>
-
-    </nav>
-
+    <header>
+        @include('layout.partials.nav')
+    </header>
 
     <main>
         @yield('main')
@@ -52,92 +17,8 @@
 
 
     <footer class="footer">
-        <div class="footer__addr">
-            <h1 class="footer__logo"><img src="{{ asset('IMG/gori_horizontal_branco.png') }}" alt="logo" width="130" /></h1>
-
-            <h2>Contacto</h2>
-
-            <address>
-                Leiria, Portugal. Escola Superior de Tecnologia e GestÃ£o<br>
-
-                <a class="footer__btn" href="mailto:gorillaboards@gmail.com">Contacte-nos</a>
-            </address>
-        </div>
-
-        <ul class="footer__nav">
-            <li class="nav__item">
-                <h2 class="nav__title">Media</h2>
-
-                <ul class="nav__ul">
-                    <li>
-                        <a href="#">Online</a>
-                    </li>
-
-                    <li>
-                        <a href="#">Print</a>
-                    </li>
-
-                    <li>
-                        <a href="#">Alternative Ads</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="nav__item nav__item--extra">
-                <h2 class="nav__title">Technology</h2>
-
-                <ul class="nav__ul nav__ul--extra">
-                    <li>
-                        <a href="#">Hardware Design</a>
-                    </li>
-
-                    <li>
-                        <a href="#">Software Design</a>
-                    </li>
-
-                    <li>
-                        <a href="#">Digital Signage</a>
-                    </li>
-
-                    <li>
-                        <a href="#">Artificial Intelligence</a>
-                    </li>
-
-                    <li>
-                        <a href="#">IoT</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="nav__item">
-                <h2 class="nav__title">Legal</h2>
-
-                <ul class="nav__ul">
-                    <li>
-                        <a href="#">Privacy Policy</a>
-                    </li>
-
-                    <li>
-                        <a href="#">Terms of Use</a>
-                    </li>
-
-                    <li>
-                        <a href="#">Sitemap</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-
-        <div class="legal">
-            <p>&copy; 2022 GorillaBoards. Todos os direitos reservados.</p>
-
-
-        </div>
+        @include('layout.partials.footer')
     </footer>
-
-
-
-
 
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
@@ -146,9 +27,6 @@
 
     <!-- MDB -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.0/mdb.min.js"></script>
-
-
-
 
 </body>
 
---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,81 @@
+<div class="footer__addr">
+    <h1 class="footer__logo"><img src="{{ asset('IMG/gori_horizontal_branco.png') }}" alt="logo" width="130" /></h1>
+
+    <h2>Contacto</h2>
+
+    <address>
+        Leiria, Portugal. Escola Superior de Tecnologia e GestÃ£o<br>
+
+        <a class="footer__btn" href="mailto:gorillaboards@gmail.com">Contacte-nos</a>
+    </address>
+</div>
+
+<ul class="footer__nav">
+    <li class="nav__item">
+        <h2 class="nav__title">Media</h2>
+
+        <ul class="nav__ul">
+            <li>
+                <a href="#">Online</a>
+            </li>
+
+            <li>
+                <a href="#">Print</a>
+            </li>
+
+            <li>
+                <a href="#">Alternative Ads</a>
+            </li>
+        </ul>
+    </li>
+
+    <li class="nav__item nav__item--extra">
+        <h2 class="nav__title">Technology</h2>
+
+        <ul class="nav__ul nav__ul--extra">
+            <li>
+                <a href="#">Hardware Design</a>
+            </li>
+
+            <li>
+                <a href="#">Software Design</a>
+            </li>
+
+            <li>
+                <a href="#">Digital Signage</a>
+            </li>
+
+            <li>
+                <a href="#">Artificial Intelligence</a>
+            </li>
+
+            <li>
+                <a href="#">IoT</a>
+            </li>
+        </ul>
+    </li>
+
+    <li class="nav__item">
+        <h2 class="nav__title">Legal</h2>
+
+        <ul class="nav__ul">
+            <li>
+                <a href="#">Privacy Policy</a>
+            </li>
+
+            <li>
+                <a href="#">Terms of Use</a>
+            </li>
+
+            <li>
+                <a href="#">Sitemap</a>
+            </li>
+        </ul>
+    </li>
+</ul>
+
+<div class="legal">
+    <p>&copy; 2022 GorillaBoards. Todos os direitos reservados.</p>
+
+
+</div>
