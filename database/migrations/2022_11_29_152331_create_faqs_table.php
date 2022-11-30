08ecb342454071384847d396291e1868184bc01d
Tmarques21 <106841669+Tmarques21@users.noreply.github.com>
2022-11-30 11:10:30 +0000

    Update 2022_11_29_152331_create_faqs_table.php
    add  faq table

--- database/migrations/2022_11_29_152331_create_faqs_table.php
+++ database/migrations/2022_11_29_152331_create_faqs_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
+            $table->string('Pergunta');
+            $table->text('Resposta');
             $table->timestamps();
         });
     }
