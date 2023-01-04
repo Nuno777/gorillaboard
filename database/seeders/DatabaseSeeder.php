6fa2dc05af649f440ac321b58894154d04d03687
Tmarques21 <106841669+Tmarques21@users.noreply.github.com>
2023-01-04 13:14:47 +0000

    edit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            userSeeder::class,
+            faqsSeeders::class,
+            desportosSeeder::class,
+        ]);
     }
 }
