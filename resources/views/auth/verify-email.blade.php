af41b5d16af4f244d8cd51aa91aa8a661d424af5
Tmarques21 <106841669+Tmarques21@users.noreply.github.com>
2023-01-04 17:00:42 +0000

    change verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,4 +1,46 @@
-<title>Email de Verificação</title>
+@extends('layouts.master')
+    @vite(['/public/css/style_login.css', '/public/js/app.js'])
+    @section('title', 'GorillaBoards')
+    @section('main')
+
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+        <div class="container">
+            <div class="forms-container">
+                <div class="signin-signup">
+                    @if (session('status') == 'verification-link-sent')
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ __(' Um novo link de verificação foi enviado para o endereço de e-mail que você forneceu durante o registro.') }}
+                    </div>
+                    @endif
+                    <form method="POST" action="{{ route('verification.send') }}">
+                        @csrf
+                        <h2 class="title">Email de Verificação</h2>
+                        <button type="submit" class="btn solid">Reenviar email de verificação</button>
+                    </form>
+                    
+                </div>
+            </div>
+
+            <div class="panels-container">
+                <div class="panel left-panel">
+                    <div class="content">
+                        <h3>Olá {{ Auth::user()->name }}</h3>
+                        <p>
+                            Precisa agora de verifiar a sua identidade!
+                        </p>
+                        <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                    <button class="btn transparent" type="submit">Logout</button>
+                    </form>
+
+                    </div>
+                    <img src="{{ asset('img/undraw_mail_re_duel.svg') }}" class="image" alt="" />
+                </div>
+        </div>
+    @endsection
+
+ {{--<title>Email de Verificação</title>
 <link href="{{ asset('img/favicon.png') }}" rel="icon">
 <link href="{{ asset('img/favicon.png ') }}" rel="apple-touch-icon">
 <x-guest-layout>
@@ -42,3 +84,4 @@
         </div>
     </x-auth-card>
 </x-guest-layout>
+ --}}
